706ece2a8bb29dfc79bb72aa646d9fc85b87f683
pemrouz <p3pedram@hotmail.com>
2016-03-29 00:32:31 +0100

    update datum

--- datum.js
+++ datum.js
@@ -1,5 +1,5 @@
 var sel = require('utilise/sel')
 
 module.exports = function datum(node){
-  return sel(node).datum()
+  return node.__data__
 }
--- utilise.js
+++ utilise.js
@@ -131,7 +131,7 @@ module.exports = function copy(from, to){
 var sel = require('utilise/sel')
 
 module.exports = function datum(node){
-  return sel(node).datum()
+  return node.__data__
 }
 },{"utilise/sel":69}],14:[function(require,module,exports){
 var is = require('utilise/is')
