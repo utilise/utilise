43ab45e9909b58c12b89e57e5b9d3aec513f02d6
pemrouz <p3pedram@hotmail.com>
2017-11-27 00:45:47 +0000

    update tdraw

--- utilise.js
+++ utilise.js
@@ -1206,7 +1206,7 @@ function stripws(d){
 function draw(host, fn, state) {
   var el = host.node ? host.node() : host
   el.state = state || {}
-  el.draw = function(d){ return fn && fn.call(el, el.state) }
+  el.draw = function(d){ return fn && fn.call(el, el, el.state) }
   el.draw()
   return host
 }
